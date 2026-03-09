d7a930fdfe329c4a3126d102de5da073c4e9f27e
LAPTOP-J9U9GBB8\curin <curini>
2026-03-09 12:34:42 +0100

    fix seed

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -103,14 +103,21 @@ async function seedRevenue() {
 
 export async function GET() {
   try {
-    const result = await sql.begin((sql) => [
-      seedUsers(),
-      seedCustomers(),
-      seedInvoices(),
-      seedRevenue(),
-    ]);
+    // const result = await sql.begin((sql) => [
+    //   seedUsers(),
+    //   seedCustomers(),
+    //   seedInvoices(),
+    //   seedRevenue(),
+    // ]);
 
-    return Response.json({ message: 'Database seeded successfully' });
+    await sql.begin((sql) =>
+      seedUsers()
+        .then(() => seedCustomers())
+        .then(() => seedInvoices())
+        .then(() => seedRevenue()),
+    );
+
+    return Response.json({ message: "Database seeded successfully" });
   } catch (error) {
     return Response.json({ error }, { status: 500 });
   }
